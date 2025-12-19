7fd16278d6793c6e4a2f5ae67f12fdc3aeeaeb6e
Mithilesh-salunkhe <mithileshsalunkhe3@gmail.com>
2025-12-19 14:53:49 +0530

    Back to original code

--- wrapper-service/src/index.ts
+++ wrapper-service/src/index.ts
@@ -29,7 +29,7 @@ app.get("/", (req: Request, res: Response) => {
 app.use(errorHandler);
 
 //Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = parseInt(process.env.PORT || "3000",10);
+app.listen(PORT,"0.0.0.0", () => {
     console.log(` Server running on http://localhost:${PORT}`);
 });
